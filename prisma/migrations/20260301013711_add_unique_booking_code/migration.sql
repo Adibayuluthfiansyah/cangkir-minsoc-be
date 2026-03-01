3d55b713a3304fc36756da12d32269aad1214dd9
Adibayu Luthfiansyah <adibayuluthfiansyah@gmail.com>
2026-03-01 23:45:53 +0700

    fix: remove invalid unique bookingcode migration  prisma

--- prisma/migrations/20260301013711_add_unique_booking_code/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex (recreate as unique instead of regular index)
-DROP INDEX IF EXISTS "bookings_booking_code_idx";
-
--- CreateIndex (as unique constraint)
-CREATE UNIQUE INDEX "bookings_booking_code_key" ON "bookings"("booking_code");
